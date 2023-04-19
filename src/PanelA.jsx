9311a339060014957d67fa7ba0c36eb391150ed2
rajpatel2511 <rajpatel25200@gmail.com>
2023-04-19 18:47:36 +0530

    19/04(06:47)

--- src/PanelA.jsx
+++ src/PanelA.jsx
@@ -2,13 +2,21 @@ import React , {useState} from "react";
 
 const PanelA = ({toggle,activePanel}) =>{
 
-console.log(activePanel)
+    
+const [show, setShow] = useState(false);
+ console.log(show)
+const handleOpen = () => {
+    setShow(!show); 
+    console.log(!show)
+     toggle(!show ? 1:null)
+  };
 
     return(<>
         <h1>P1</h1>
-        <button onClick={()=>toggle(1)}>click</button>
+        <button onClick={handleOpen}>click</button>
 
-        {activePanel===1 ? <p>this is 1st panel <hr /></p> : null}
+
+        {activePanel===1 ? <p>this is 1st panel </p> : null}
      
         </>)
 }
--- src/PanelC.jsx
+++ src/PanelC.jsx
@@ -7,7 +7,7 @@ const PanelB = ({toggle,activePanel}) =>{
         <h1>P3</h1>
         <button onClick={()=>toggle(3)} >click</button>
 
-        {activePanel===3 ?  <p>this is 3rd panel <hr />     </p>:null}
+        {activePanel===3 ?  <p>this is 3rd panel </p>:null}
 
 
         </>)
